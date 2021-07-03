2b49cea6635d528c5bc08b9c33d0193e3028b998
Christian Paranas <christiannparanas@gmail.com>
2021-07-03 17:48:05 +0800

    remote backend

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   env: {
-    BACKEND_BASEURL: "http://localhost:3001",
+    BACKEND_BASEURL: "https://studportalscrape.herokuapp.com",
   }
 
 }
+
+// https://studportalscrape.herokuapp.com/
+// http://localhost:3001
