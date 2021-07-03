0f6994097bfe83ebc6840d631e28a10e0c659412
Christian Paranas <christiannparanas@gmail.com>
2021-07-03 23:04:23 +0800

    frop

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,15 +9,32 @@ export default function Nav( props ) {
 	const [sidebar, setSidebar] = useState('nav_sidebar')
 	const [clickoutside, setClickoutside] = useState('clickoutside_overlay')
 
+
+	const [dropOv, setDropOv] = useState('dropBox')
+	const [dropIco, setDropIco] = useState("far fa-sort-down")
+
+
 	const closeSidebar = () => {
 		setClickoutside('clickoutside_overlay')
 		setSidebar('nav_sidebar')
 		setOpen(false)
+
 	}
 
 	const openSidebar = () => {
 		setClickoutside('clickoutside_overlay show_clickoutside_overlay')
 		setSidebar('nav_sidebar show_nav_sidebar')
+	}
+
+	const openDrop = () => {
+		if(dropOv == "dropBox") {
+			setDropOv("dropBox showDrop")
+			setDropIco("far fa-sort-up")
+		} else {
+			setDropIco("far fa-sort-down")
+			setDropOv("dropBox")
+		}
+		
 	}
 
 	useEffect(() => {
@@ -40,9 +57,22 @@ export default function Nav( props ) {
 				<div className="nav_options">
 					<Hamburger toggled={isOpen} toggle={setOpen} />
 				</div>
-				<div className="nav_options">
+				<div className="nav_options nav_click" onClick={openDrop}>
 					<img src="https://apps.evsu.edu.ph/assets/img/students/2018/2018-02022-e121351d79e353eebe8338eab204a2fe.jpg" alt="" />
-					christian <i className="far fa-sort-down"></i>
+					christian <i className={dropIco}></i>
+				</div>
+
+				<div className={dropOv}>
+					<div className="dropImg">
+						<img src="https://apps.evsu.edu.ph/assets/img/students/2018/2018-02022-e121351d79e353eebe8338eab204a2fe.jpg" alt="" />
+						<p>Christian</p>
+					</div>
+					<div className="dropOp">
+						<div className="op"><i className="fal fa-image-polaroid"></i> Update Photo</div>
+						<div className="op"><i className="fal fa-cog"></i> Account Settings</div>
+						<div className="op oo"><i className="fal fa-lock"></i> Change Password</div>
+						<div className="op out">Logout <i className="fad fa-sign-out"></i> </div>
+					</div>
 				</div>
 			</div>
 
